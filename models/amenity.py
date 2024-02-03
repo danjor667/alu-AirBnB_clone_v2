8deae4017778314a6a7ed97adb6d65181cffc255
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2024-02-03 18:21:24 +0200

    Added object initializers

--- models/amenity.py
+++ models/amenity.py
@@ -13,6 +13,9 @@ class Amenity(BaseModel, Base):
     if storage_t == "db":
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity")
     else:
         name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity object"""
+        super().__init__(*args, **kwargs)
