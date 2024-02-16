bb2e067693bac9bf59ff03ff2b19a7576d1b61d8
danjor667 <jordannguepi@gmail.com>
2024-02-16 21:26:38 +0200

    fix 3

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html><h1>Holberton School</h1></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
