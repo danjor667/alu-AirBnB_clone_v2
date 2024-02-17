402934e1a85cbf0de9b98e7ca763c02681b86b98
danjor667 <jordannguepi@gmail.com>
2024-02-17 02:34:25 +0200

    fix 4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# Write a Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<h1>Holberton School</h1>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+apt-get update
+apt-get -y install nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "<h1>Holberton School</h1>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
