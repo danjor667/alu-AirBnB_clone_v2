9caf826697c1b24c9039746a1776abc9376cabdc
danjor667 <jordannguepi@gmail.com>
2024-02-18 22:51:01 +0200

    done1

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+doc
+"""
+
+from fabric.api import put, run, env, local
+from os.path import exists
+from datetime import datetime
+from os.path import isdir
+
+env.hosts = ['18.205.245.245', '54.208.57.96']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/school'
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        print("archive not present")
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def deploy():
+    file = do_pack()
+    if not file:
+        return False
+    else:
+        return do_deploy(file)
+if __name__ == "__main__":
+    deploy()
