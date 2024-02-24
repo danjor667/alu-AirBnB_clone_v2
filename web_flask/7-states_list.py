a07057d7d29513b2e50dc717e7ea0fcd81b38836
danjor667 <jordannguepi@gmail.com>
2024-02-24 09:51:20 +0200

    state_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Starting a Flask app
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(exeception):
+    """force closure of the current db session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """doc
+    """
+    states = storage.all(State).values().sort(key=lambda state: state.name)
+    return render_template('7-state_list.html', states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
