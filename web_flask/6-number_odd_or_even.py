8791a5a200b808e6bec6ad64c656ce1a6af1ce60
danjor667 <jordannguepi@gmail.com>
2024-02-24 07:42:14 +0200

    fixing templates1

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,7 +53,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """
     Displays an HTML page if n is an integer
